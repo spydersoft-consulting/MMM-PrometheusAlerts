91e0768bcf926ad85928f4e54316ee9499149c3b
Matt Gerega <gerega@gmail.com>
2023-10-16 18:11:06 -0400

    Fixed node-fetch issues and pipeline adjustments (#7)

--- src/types/Prometheus.ts
+++ src/types/Prometheus.ts
@@ -1,3 +1,11 @@
+export type AlertResponse = {
+  data: AlertResponseData;
+};
+
+export type AlertResponseData = {
+  alerts: PrometheusAlert[];
+};
+
 export type PrometheusAlert = {
   labels: Record<string, string>;
   annotations: Record<string, string>;
